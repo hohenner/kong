f314f5a0255395ae3c7d640432bd6c36292e8c5a
Michael Martin <3277009+flrgh@users.noreply.github.com>
2022-05-13 09:39:13 -0700

    feat(balancer) add uri_capture consistent hash source

--- kong/db/migrations/core/016_280_to_300.lua
+++ kong/db/migrations/core/016_280_to_300.lua
@@ -18,6 +18,24 @@ return {
           -- Do nothing, accept existing state
         END;
       $$;
+
+      -- add new hash_on_uri_capture field to upstreams
+      DO $$
+        BEGIN
+          ALTER TABLE IF EXISTS ONLY "upstreams" ADD "hash_on_uri_capture" TEXT;
+        EXCEPTION WHEN DUPLICATE_COLUMN THEN
+          -- Do nothing, accept existing state
+        END;
+      $$;
+
+      -- add new hash_fallback_uri_capture field to upstreams
+      DO $$
+        BEGIN
+          ALTER TABLE IF EXISTS ONLY "upstreams" ADD "hash_fallback_uri_capture" TEXT;
+        EXCEPTION WHEN DUPLICATE_COLUMN THEN
+          -- Do nothing, accept existing state
+        END;
+      $$;
     ]],
   },
 
@@ -28,6 +46,12 @@ return {
 
       -- add new hash_fallback_query_arg field to upstreams
       ALTER TABLE upstreams ADD hash_fallback_query_arg text;
+
+      -- add new hash_on_uri_capture field to upstreams
+      ALTER TABLE upstreams ADD hash_on_uri_capture text;
+
+      -- add new hash_fallback_uri_capture field to upstreams
+      ALTER TABLE upstreams ADD hash_fallback_uri_capture text;
     ]],
   },
 }
